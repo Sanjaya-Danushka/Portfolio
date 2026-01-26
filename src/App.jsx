275f398e87cec2daf66526ce3cd590217d3c9e7f
Sanjaya Danushka <dsanjaya712@gmail.com>
2026-01-26 10:18:13 +0530

    implement main application structure with navbar and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./layout/navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
+import Projects from "./sections/Projects.jsx";
+import Experience from "./sections/Experience.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Contacts from "./sections/Contacts.jsx";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contacts />
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/Contacts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contacts = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default contacts
